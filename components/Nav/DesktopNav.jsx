3770116500190841f01dc280f06b12073c25358b
Basit Saeed <69200255+winepine@users.noreply.github.com>
2022-01-22 21:05:46 +0500

    Never Use ChakraUI/Link

--- components/Nav/DesktopNav.jsx
+++ components/Nav/DesktopNav.jsx
@@ -1,100 +1,102 @@
 import {
-    Box,
-    Stack,
-    Link,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-import DesktopSubNav from './DesktopSubNav';
-  const NAV_ITEMS = [
-    {
-      label: 'Home',
-      href:"/"
-    },
-    {
-      label: 'Skills',
-      children: [
-        {
-          label: 'All Technologies',
-          subLabel: 'Languages/Tools/Technologies',
-          href: '#',
-        },
-        {
-          label: 'Web Frontend',
-          subLabel: 'Frontend technologies',
-          href: '#',
-        },
-        {
-          label: 'Web Backend',
-          subLabel: 'Backend technologies',
-          href: '#',
-        },
-        {
-          label: 'Database & Cloud',
-          subLabel: 'Databases and deployment technologies',
-          href: '#',
-        },
-      ],
-    },
-    {
-      label: 'Education & Certifications',
-      href: '#',
-    },
-    {
-      label: 'Projects',
-      href: '/',
-    },
-    {
-      label: 'About',
-      href: '/about',
-    },
-  ];
+  Box,
+  Stack,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import DesktopSubNav from "./DesktopSubNav";
+const NAV_ITEMS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Skills",
+    children: [
+      {
+        label: "All Technologies",
+        subLabel: "Languages/Tools/Technologies",
+        href: "#",
+      },
+      {
+        label: "Web Frontend",
+        subLabel: "Frontend technologies",
+        href: "#",
+      },
+      {
+        label: "Web Backend",
+        subLabel: "Backend technologies",
+        href: "#",
+      },
+      {
+        label: "Database & Cloud",
+        subLabel: "Databases and deployment technologies",
+        href: "#",
+      },
+    ],
+  },
+  {
+    label: "Education & Certifications",
+    href: "#",
+  },
+  {
+    label: "Projects",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+];
 const DesktopNav = () => {
-    const linkColor = useColorModeValue('gray.600', 'gray.200');
-    const linkHoverColor = useColorModeValue('gray.800', 'white');
-    const popoverContentBgColor = useColorModeValue('white', 'gray.800');
-  
-    return (
-      <Stack direction={'row'} spacing={4}>
-        {NAV_ITEMS.map((navItem) => (
-          <Box key={navItem.label}>
-            <Popover trigger={'hover'} placement={'bottom-start'}>
-              <PopoverTrigger>
-                <Link
-                  p={2}
-                  href={navItem.href ?? '#'}
-                  fontSize={'sm'}
-                  fontWeight={500}
-                  color={linkColor}
-                  _hover={{
-                    textDecoration: 'none',
-                    color: linkHoverColor,
-                  }}>
-                  {navItem.label}
-                </Link>
-              </PopoverTrigger>
-  
-              {navItem.children && (
-                <PopoverContent
-                  border={0}
-                  boxShadow={'xl'}
-                  bg={popoverContentBgColor}
-                  p={4}
-                  rounded={'xl'}
-                  minW={'sm'}>
-                  <Stack>
-                    {navItem.children.map((child) => (
-                      <DesktopSubNav key={child.label} {...child} />
-                    ))}
-                  </Stack>
-                </PopoverContent>
-              )}
-            </Popover>
-          </Box>
-        ))}
-      </Stack>
-    );
-  };
-  export default DesktopNav;
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("gray.800", "white");
+  const popoverContentBgColor = useColorModeValue("white", "gray.800");
+
+  return (
+    <Stack direction={"row"} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={"hover"} placement={"bottom-start"}>
+            <PopoverTrigger>
+              <Link
+                p={2}
+                href={navItem.href ?? "#"}
+                fontSize={"sm"}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: "none",
+                  color: linkHoverColor,
+                }}
+              >
+                {navItem.label}
+              </Link>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={"xl"}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={"xl"}
+                minW={"sm"}
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+export default DesktopNav;
--- components/Nav/MobileNavItem.jsx
+++ components/Nav/MobileNavItem.jsx
@@ -1,67 +1,71 @@
 import {
-    Flex,
-    Text,
-    Stack,
-    Collapse,
-    Icon,
-    Link,
-    useColorModeValue,
-    useDisclosure,
-    Button,
-  } from '@chakra-ui/react';
-  import {
-    ChevronDownIcon,
-  } from '@chakra-ui/icons';
-  import Router from 'next/router'
+  Flex,
+  Text,
+  Stack,
+  Collapse,
+  Icon,
+  useColorModeValue,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import Router from "next/router";
 const MobileNavItem = ({ label, children, href }) => {
-    const { isOpen, onToggle } = useDisclosure();
-    
-    return (
-      <Stack spacing={4} onClick={children && onToggle}>
-        <Flex
-          py={2}
-          as={Button}
-          onClick={()=>{if(href){Router.replace(href)}}}
-          // href={href ?? '#'}
-          justify={'space-between'}
-          align={'center'}
-          _hover={{
-            textDecoration: 'none',
-          }}>
-          <Text
-            fontWeight={600}
-            color={useColorModeValue('gray.600', 'gray.200')}>
-            {label}
-          </Text>
-          {children && (
-            <Icon
-              as={ChevronDownIcon}
-              transition={'all .25s ease-in-out'}
-              transform={isOpen ? 'rotate(180deg)' : ''}
-              w={6}
-              h={6}
-            />
-          )}
-        </Flex>
-  
-        <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-          <Stack
-            mt={2}
-            pl={4}
-            borderLeft={1}
-            borderStyle={'solid'}
-            borderColor={useColorModeValue('gray.200', 'gray.700')}
-            align={'start'}
-            >
-            {children &&
-              children.map((child) => (
-                <Link key={child.label} py={2} href={child.href}>
-                  {child.label}
-                </Link>
-              ))}
-          </Stack>
-        </Collapse>
-      </Stack>
-    );
-  };
-  export default MobileNavItem
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Flex
+        py={2}
+        as={Button}
+        onClick={() => {
+          if (href) {
+            Router.replace(href);
+          }
+        }}
+        // href={href ?? '#'}
+        justify={"space-between"}
+        align={"center"}
+        _hover={{
+          textDecoration: "none",
+        }}
+      >
+        <Text
+          fontWeight={600}
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={"all .25s ease-in-out"}
+            transform={isOpen ? "rotate(180deg)" : ""}
+            w={6}
+            h={6}
+          />
+        )}
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
+          {children &&
+            children.map((child) => (
+              <Link key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Link>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+export default MobileNavItem;
