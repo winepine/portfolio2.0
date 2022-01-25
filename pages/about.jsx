e54c0e4ac7b2d616d15670a7c26a5f418ae9846a
Basit Saeed <69200255+winepine@users.noreply.github.com>
2022-01-25 19:27:26 +0500

    Animations Improved

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 const Home = () => (
   <motion.div
     initial={{ opacity: 0 }}
-    animate={{ opacity: 1, transition: { duration: 1 } }}
+    animate={{ opacity: 1, transition: { duration: 0.5 } }}
     exit={{ opacity: 0 }}
   >
     <div style={{ paddingTop: "40vh" }}>About Page</div>
--- pages/skills/frontend.jsx
+++ pages/skills/frontend.jsx
@@ -1,31 +1,54 @@
 import { Box, Text, Flex, useColorModeValue } from "@chakra-ui/react";
 import Tech from "../../components/Tech";
 import { motion } from "framer-motion";
-const MotioxFlex = motion(Flex);
-const MotioxBox = motion(Box);
-const MotioxText = motion(Text);
+const MotionFlex = motion(Flex);
+const MotionBox = motion(Box);
+const MotionText = motion(Text);
+const container = {
+  init: {
+    opacity: 0,
+  },
+  anim: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
+const childanims = {
+  init: {
+    opacity: 0,
+  },
+  anim: {
+    opacity: 1,
+  },
+};
 const Frontend = () => {
   return (
-    <MotioxBox
-      initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-        transition: { duration: 1 },
-      }}
-      exit={{ opacity: 0 }}
-    >
+    <MotionBox overflow="hidden">
       <Box pt="5rem" minHeight="95vh">
         <Flex
           mt={{ base: "0", md: "10vh" }}
           flexDirection={{ base: "column", xl: "row" }}
         >
-          <MotioxFlex
+          <MotionFlex
             flex={13}
             flexDirection="column"
             justifyContent="center"
             flexWrap="wrap"
+            initial={{ opacity: 0, x: -200 }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 0.5, type: "spring", stiffness: 50 },
+            }}
+            exit={{
+              opacity: 0,
+              x: -200,
+              transition: { duration: 0.5, type: "spring", stiffness: 50 },
+            }}
           >
-            <MotioxText
+            <MotionText
               fontFamily="Bebas Neue, cursive"
               fontSize={{ base: "5rem", lg: "14vw" }}
               ml="5vh"
@@ -33,8 +56,8 @@ const Frontend = () => {
               fontWeight={800}
             >
               FRONTEND
-            </MotioxText>
-            <Text
+            </MotionText>
+            <MotionText
               fontFamily="Bebas Neue, cursive"
               fontSize={{ base: "2rem", lg: "5vw" }}
               ml="5vh"
@@ -42,10 +65,35 @@ const Frontend = () => {
               fontWeight={400}
             >
               Technologies
-            </Text>
-          </MotioxFlex>
+            </MotionText>
+          </MotionFlex>
           <Flex flex={1}></Flex>
-          <Flex flex={17} justifyContent="center" flexWrap="wrap">
+          <MotionFlex
+            initial={{ opacity: 0, y: 100 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 0.3,
+                duration: 0.5,
+                type: "spring",
+                stiffness: 50,
+              },
+            }}
+            exit={{
+              opacity: 0,
+              y: 100,
+              transition: {
+                duration: 0.5,
+                delay: 0.2,
+                type: "spring",
+                stiffness: 50,
+              },
+            }}
+            flex={17}
+            justifyContent="center"
+            flexWrap="wrap"
+          >
             <Tech text="HTML" imgUrl="/html.png" />
             <Tech text="CSS" imgUrl="/css.png" />
             <Tech text="JavaScript" imgUrl="/js.png" />
@@ -56,7 +104,7 @@ const Frontend = () => {
             <Tech text="Framer motion" imgUrl="/framer.svg" />
             <Tech text="React Router" imgUrl="/router.jpg" />
             <Tech text="React Spring" imgUrl="/spring.png" />
-          </Flex>
+          </MotionFlex>
         </Flex>
       </Box>
       <Box
@@ -68,7 +116,7 @@ const Frontend = () => {
       >
         <Text fontWeight={100}>© 2022 Basit Saeed. All Rights Reserved.</Text>
       </Box>
-    </MotioxBox>
+    </MotionBox>
   );
 };
 export default Frontend;
